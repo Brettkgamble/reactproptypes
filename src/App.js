974cf6295d33d766d4e2b28f70e9e490484c988c
brettkgamble@gmail.com <5413CrossFit2018>
2020-11-29 11:13:58 -0700

    0001 - first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+// Components
+import HomeComponent from "./components/Home";
+import ProfileComponent from "./components/Profile";
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <div className='anim_index'>
+                    <Link to="/" exact="true">Home</Link>
+                    <Link to="/profile">Profile</Link>
+                </div>
+                <Route path="/" exact component={HomeComponent}></Route>
+                <Route path="/profile" exact component={ProfileComponent}></Route>
+            </div>
+        </BrowserRouter>
+    )
+}
+
+export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class ProfileComponent extends Component {
+
+    render() {
+        console.log(this.props)
+        return(
+            <>
+                Profile
+            </>
+        )
+    }
+
+}
+
+ProfileComponent.propTypes = {
+    name: PropTypes.string,
+    lastname: PropTypes.string,
+    age: PropTypes.number,
+    hobbies: PropTypes.array,
+    spanish: PropTypes.bool,
+    message: PropTypes.func,
+    car: PropTypes.object,
+    mother: PropTypes.string
+}
+
+export default ProfileComponent;
